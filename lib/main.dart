df24b6d7dd480b4cdad6176681c1f976b52c4f7c
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-02-19 11:55:39 +0800

    lesson 36

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,46 @@ void main(){
 }
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hahaha'),);
+  
+  var questionIndex = 0;
+
+  void answerQuestion(){
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+    print('Answer Choosen');
   }
-}
+
+  Widget build(BuildContext context){
+
+    var questions = [
+      "What's your name?",
+      "What's your age?",
+      "How old are you?"
+    ];
+
+    return MaterialApp(home: 
+      Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion
+            ),
+            RaisedButton(
+              child: Text('Answer 2'), 
+              onPressed: () => print('Hahaha')
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion
+            )
+          ],
+        ),
+      )
+    );
+  }
+} 
